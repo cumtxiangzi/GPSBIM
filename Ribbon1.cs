78ceb947464f8c9a175c51edfd609ac2d6d9b2e2
cumtxiangzi <1059657839@qq.com>
2021-12-06 17:18:27 +0800

    Initial commit

---
+++ Ribbon1.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Excel = Microsoft.Office.Interop.Excel;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Excel;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace GPSBIM
+{
+    public partial class Ribbon1
+    {
+        Excel.Application excelApp;
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+            excelApp = Globals.ThisAddIn.Application;
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            int i = 1;
+            int j = 1;
+            foreach (Excel.Range rang in excelApp.Selection)
+            {
+                string cellContent = excelApp.Cells[rang.Row, rang.Column].Value;
+                string modifyCellContent;
+                if (cellContent != null && cellContent != "")
+                {
+                    modifyCellContent = Regex.Replace(cellContent, "[0-9]", "", RegexOptions.IgnoreCase) + i.ToString().PadLeft(2, '0');
+                    excelApp.Cells[rang.Row, rang.Column].Value = modifyCellContent;
+                    i++;
+                }
+
+                j++;
+                if (j == 500)
+                {
+                    break;
+                }
+            }
+            MessageBox.Show("成功修改" + i.ToString() + "处!", "GPSBIM", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private void button2_Click(object sender, RibbonControlEventArgs e)
+        {
+            string cellContent = excelApp.Cells[2, 4].Value;
+            string modifyCellContent = FilterEN(cellContent).Replace("-", "");
+
+            int j = 0;
+            for (int i = 7; i < 500; i++)
+            {
+                string cell = excelApp.Cells[i, 1].Value;
+                if (cell != null && cell != "" && (FilterCH(cell) == "" || FilterCH(cell) == null))
+                {
+                    excelApp.Cells[i, 1].Value = modifyCellContent;
+                    j++;
+                }
+            }
+
+            Excel.Worksheet eltSheet = excelApp.ActiveWorkbook.ActiveSheet;
+            eltSheet.Name = modifyCellContent;
+
+            string projectNum = FilterEN(excelApp.Cells[1, 4].Value).Replace("-", "");
+            eltSheet.PageSetup.LeftFooter = "&\"Arial\"" + "&16" + " " + projectNum + "-" + modifyCellContent + "-" + "WD-ELT";
+
+            MessageBox.Show("成功修改子项车间编号" + j.ToString() + "处!" + "\r" + "\r" + "成功修改页脚！", "GPSBIM", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        public string FilterEN(string inputValue)
+        {
+            if (Regex.IsMatch(inputValue, "[A-Za-z0-9\u9fa5-]+"))
+            {
+                return Regex.Match(inputValue, "[A-Za-z0-9\u9fa5-]+").Value;
+            }
+            return "";
+        }
+        public string FilterCH(string inputValue)
+        {
+            if (Regex.IsMatch(inputValue, "[\u4e00-\u9fa5]+"))
+            {
+                return Regex.Match(inputValue, "[\u4e00-\u9fa5]+").Value;
+            }
+            return "";
+        }
+    }
+}
